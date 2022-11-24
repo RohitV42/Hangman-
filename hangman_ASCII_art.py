bd2912c2ef367ce890589af744fe0ecbf2c68cf5
Camille Francis <110306796+Cams-Plan@users.noreply.github.com>
2022-11-24 23:41:04 +0000

    Create hangman_ASCII_art.py

---
+++ hangman_ASCII_art.py
@@ -0,0 +1,12 @@
+## Diagram for 6 lives hangman game.
+diagram = [
+    "   +---+\n       |\n       |\n       |\n      ===\n",
+    "   +---+\n   O   |\n       |\n       |\n      ===\n",
+    "   +---+\n   O   |\n   |   |\n       |\n      ===\n",
+    "   +---+\n   O   |\n  /|   |\n       |\n      ===\n",
+    "   +---+\n   O   |\n  /|\  |\n       |\n      ===\n",
+    "   +---+\n   O   |\n  /|\  |\n  /    |\n      ===\n",
+    "   +---+\n   O   |\n  /|\  |    [GAME OVER! x_X] \n  / \  |\n      ===\n"
+    ]
+
+#BY CAMILLE @cams-plan on Github
